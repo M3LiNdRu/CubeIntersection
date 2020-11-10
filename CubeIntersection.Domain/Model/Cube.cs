e73de5a87c8e154e7f0b9ab8ac07d1e7b0a11be0
Roger Calaf <rogercalaf@gmail.com>
2020-11-10 21:45:33 +0100

    Initial commit

---
+++ CubeIntersection.Domain/Model/Cube.cs
@@ -0,0 +1,19 @@
+﻿using CubeIntersection.Domain.Model;
+
+namespace CubeIntersection.Domain
+{
+    public class Cube
+    {
+        public Cube(Point center, double edgeLength)
+        {
+            Width = new Edge(center.X, edgeLength);
+            Height = new Edge(center.Y, edgeLength);
+            Depth = new Edge(center.Z, edgeLength);
+        }
+
+        private Edge Width;
+        private Edge Height;
+        private Edge Depth;
+
+    }
+}
---
+++ CubeIntersection.Domain/Model/Edge.cs
@@ -0,0 +1,14 @@
+﻿namespace CubeIntersection.Domain.Model
+{
+    public class Edge
+    {
+        public Edge(double center, double length)
+        {
+            Start = center - length / 2.0;
+            End = center + length / 2.0;
+        }
+
+        private double Start;
+        private double End;
+    }
+}
